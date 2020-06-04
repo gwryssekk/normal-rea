6c840554dea6bb6a55d3f52b65e728088e0da390
Gary Russell <rugary@amazon.com>
2020-06-04 16:02:36 -0700

    encore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          just a normal react app.  pushed again.  And again.
+          just a normal react app.  pushed again.  And again.Encore
         </p>
         <a
           className="App-link"
