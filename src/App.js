fe5dc51122852e39d183b0f840b0b90c2cc4fac9
Gary Russell <rugary@amazon.com>
2020-06-03 20:44:35 -0700

    asdfa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          just a normal react app.  pushed again
+          just a normal react app.  pushed again.  And again.
         </p>
         <a
           className="App-link"
