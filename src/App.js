4c63bcc1b471edd0ce133c0f42ad4d3488f009c3
Gary Russell <rugary@amazon.com>
2020-06-04 20:28:53 -0700

    Please work

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          just a normal react app.  pushed again.  And again.Encore
+          just a normal react app.  pushed again.  And again.Encore. Please work.
         </p>
         <a
           className="App-link"
